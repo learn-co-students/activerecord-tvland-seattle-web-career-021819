6783bc19733673145031dd05935d378e6efb86d5
github username <github email address>
2019-03-03 09:44:37 -0800

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  # sql equivalent of "SELECT character.name, show.name FROM characters
+  # WHERE actor_id = show_id"
+  def list_roles
+    self.characters.map do |character|
+      character.name + " - " + character.show.name
+    end
+  end
+
 end
