256836d06f5a8419f778cf23868bcb5994cac6cf
clairemuller <clairermuller@gmail.com>
2019-03-04 22:58:56 -0800

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 class Character < ActiveRecord::Base
-  
+  belongs_to :show
+  belongs_to :actor
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(name_hash)
+    self.show = Show.new(name: name_hash[:name])
+  end
+
+  def build_network(call_letters_hash)
+    binding.pry
+    show = self.build_show
+    show.network = Network.new(call_letters: call_letters_hash[:call_letters])
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(call_letters_hash)
+    self.network = Network.new(call_letters: call_letters_hash[:call_letters])
+  end
+
 end
